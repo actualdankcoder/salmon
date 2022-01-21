3527f686c59844367ad9f4de8c0a4a010b218273
actualdankcoder <zubi2048@gmail.com>
2022-01-21 17:49:17 +0530

    ✨Added CORS depd✨

--- server.go
+++ server.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
 )
 
 func RunServer() {	
 	r := gin.Default()
+	r.Use(cors.Default())
 	r.GET("/", func(c *gin.Context) {
 		dat := LoadJSON()
 		c.IndentedJSON(200, dat)
